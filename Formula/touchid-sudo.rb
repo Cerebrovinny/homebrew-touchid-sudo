fa81029c1a8e949eab34f70102cdb00848af7de2
Cerebrovinny <vinny.cardoso96@gmail.com>
2023-05-06 07:59:44 -0300

    added touch id formula

--- Formula/touchid-sudo.rb
+++ Formula/touchid-sudo.rb
@@ -1,7 +1,7 @@
 class TouchidSudo < Formula
     desc "Enable TouchID authentication for sudo on macOS"
     homepage "https://github.com/Cerebrovinny/homebrew-touchid-sudo"
-    url "https://github.com/Cerebrovinny/homebrew-touchid-sudo/archive/refs/tags/v1.0.1.tar.gz"
+    url "https://github.com/Cerebrovinny/homebrew-touchid-sudo/archive/refs/tags/v1.0.0.tar.gz"
     sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     license "MIT"
   
